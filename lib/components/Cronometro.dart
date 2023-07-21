6d1c877fae74718b4261fcac6650e16bf4913c58
Samiel <samuelwachholz@gmail.com>
2023-07-21 13:48:09 -0300

    feat: gerenciamento de estado aplicado ao cronometro

--- lib/components/Cronometro.dart
+++ lib/components/Cronometro.dart
@@ -13,34 +13,36 @@ class Cronometro extends StatelessWidget {
   Widget build(BuildContext context) {
     final store = Provider.of<PomodoroStore>(context);
 
-    return Container(
-      color: store.estaTrabalhando() ? Colors.red : Colors.green,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            store.estaTrabalhando() ? 'Hora de Trabalhar' : 'Hora de descansar',
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
+    return Observer(
+      builder: (_) => Container(
+        color: store.estaTrabalhando() ? Colors.red : Colors.green,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              store.estaTrabalhando()
+                  ? 'Hora de Trabalhar'
+                  : 'Hora de descansar',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
-            style: TextStyle(
-              fontSize: 120,
-              color: Colors.white,
+            SizedBox(
+              height: 20,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Observer(
-            builder: (_) => Row(
+            Text(
+              '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
+              style: TextStyle(
+                fontSize: 120,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 if (!store.iniciado)
@@ -71,8 +73,8 @@ class Cronometro extends StatelessWidget {
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
