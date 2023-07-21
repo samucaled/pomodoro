3dc24ebb1b34cdee88451bb445795b5e374c6e5c
Samiel <samuelwachholz@gmail.com>
2023-07-21 10:57:18 -0300

    feat: componente cronometro

--- lib/pages/Pomodoro.dart
+++ lib/pages/Pomodoro.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:pomodoro/components/Cronometro.dart';
 import 'package:pomodoro/components/EntradaTempo.dart';
 
 class Pomodoro extends StatelessWidget {
@@ -10,20 +11,26 @@ class Pomodoro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Pomodoro'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              EntradaTempo(
-                titulo: 'Trabalho',
-                valor: 25,
-              ),
-              EntradaTempo(
-                titulo: 'Descanso',
-                valor: 5,
-              ),
-            ],
+          Expanded(
+            child: Cronometro(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                EntradaTempo(
+                  titulo: 'Trabalho',
+                  valor: 25,
+                ),
+                EntradaTempo(
+                  titulo: 'Descanso',
+                  valor: 5,
+                ),
+              ],
+            ),
           ),
         ],
       ),
